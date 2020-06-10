154068595074e9fb6f1c5a69e4f69672706d0b1d
Vaibhav <varora@catalystone.com>
2020-06-10 22:56:58 +0530

    fix: 🐛 same users were getting cloned in server

--- src/multiplayer/server.js
+++ src/multiplayer/server.js
@@ -8,17 +8,18 @@ server.on('connection', (webSocket) => {
     webSocket.on('message', (message) => {
         const incomingUser = JSON.parse(message);
 
-        let findIncomingUserInList = users.find(user => user.id === incomingUser.id)
-        const isNewUser = findIncomingUserInList !== undefined;
+        const indexOfIncomingUser = users.findIndex(user => user.id === incomingUser.id);
+        const isNewUser = indexOfIncomingUser < 0;
 
         if (isNewUser) {
             users.push(incomingUser);
         } else {
-            findIncomingUserInList = incomingUser;
+            users.splice(indexOfIncomingUser, 1);
+            users.push(incomingUser);
         }
 
         // forward details of user back to all web sockets opened in browser
         // exaple - User 2 detail is here. it will be sent to User 1. And User 1 will update snake game accordingly
-        webSocket.send(JSON.stringify(findIncomingUserInList));
+        webSocket.send(JSON.stringify(incomingUser));
     });
 });
