fb4a9f8c0bf9d23c3ccda61fa6bc564058cf9b61
Vaibhav <varora@catalystone.com>
2020-06-11 21:03:31 +0530

    fix: 🐛 broadcast message to all clients

--- src/multiplayer/server.js
+++ src/multiplayer/server.js
@@ -1,10 +1,11 @@
 const WS = require('ws');
 
 const server = new WS.Server({ port: 8080 });
-server.on('connection', (webSocket) => {
-    // keep a list of users playing snake game
-    const users = [];
 
+// keep a list of users playing snake game
+const users = [];
+
+server.on('connection', (webSocket) => {
     webSocket.on('message', (message) => {
         const incomingUser = JSON.parse(message);
 
@@ -20,6 +21,10 @@ server.on('connection', (webSocket) => {
 
         // forward details of user back to all web sockets opened in browser
         // exaple - User 2 detail is here. it will be sent to User 1. And User 1 will update snake game accordingly
-        webSocket.send(JSON.stringify(incomingUser));
+        server.clients.forEach((client) => {
+            if (client !== server && client.readyState === WS.OPEN) {
+                client.send(JSON.stringify(users));
+            }
+        });
     });
 });
