a07c49a9c5879d3d1e359b1bf2dc284ab04329c8
Vaibhav <varora@catalystone.com>
2020-06-17 22:47:57 +0530

    feat: 🎸 WIP::setup other users snake visibility on board

--- src/Components/gameBoard/gameBoard.js
+++ src/Components/gameBoard/gameBoard.js
@@ -1,13 +1,53 @@
-import React, { useState } from 'react';
-import './style.css';
-import Snake from '../snake/snake';
-import Food from '../food/food';
+import React, { useEffect, useState } from 'react';
 import { getRandomCoords } from '../../utils/utils';
+import { SERVER_URL } from '../../utils/variables';
+import Food from '../food/food';
 import LeaderBoard from '../loaderboard/loaderboard';
+import Snake from '../snake/snake';
+import './style.css';
+const { randomString } = require('../../string-utility');
+
+
+const registerUser = (webSocket) => {
+  const user = { id: randomString() };
+  webSocket.onopen = () => {
+    webSocket.send(JSON.stringify(user));
+  }
+  return user;
+}
 
 const GameBoard = (props) => {
   const [foodCoords, setFoodCoords] = useState(getRandomCoords());
   const [score, setScore] = useState(0);
+  const [otherUsersSnake, setOtherUsersSnake] = useState(null);
+
+  const webSocket = new WebSocket(SERVER_URL);
+  const currentUser = registerUser(webSocket);
+
+  const otherUsersID = props.usersID || [];
+
+  useEffect(() => {
+    webSocket.addEventListener('message', updateOtherUsersData);
+    return closeWebSocket;
+  })
+
+  const closeWebSocket = () => {
+    webSocket.close();
+  }
+
+  const updateOtherUsersData = (message) => {
+    const usersData = JSON.parse(message.data);
+    let filteredOtherUsers;
+
+    if (Array.isArray(usersData)) {
+      filteredOtherUsers = usersData.filter(user => otherUsersID.indexOf(user.id) >= 0);
+    }
+
+    if (Array.isArray(filteredOtherUsers)) {
+      const otherUsersSnake = filteredOtherUsers.map(users => users.snake);
+      setOtherUsersSnake(otherUsersSnake);
+    }
+  }
 
   return (
     <>
@@ -15,6 +55,10 @@ const GameBoard = (props) => {
         <div className="col-md-10">
           <div className="game-window">
             <Snake foodCoords={foodCoords} foodCoordsSetter={setFoodCoords} score={score} scoreSetter={setScore} setGameOver={props.setGameOver} />
+            {
+              otherUsersSnake &&
+              otherUsersSnake.map(snake => <Snake coords={snake.coords} />)
+            }
             <Food foodCoordinates={foodCoords} />
           </div>
         </div>
